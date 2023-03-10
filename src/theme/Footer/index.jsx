e085798fb16864642fd69a87c3f19c82020d0254
Asmit2952 <asmitbm2952002@gmail.com>
2023-03-10 20:26:18 +0530

    fix light/dark theme img

--- src/theme/Footer/index.jsx
+++ src/theme/Footer/index.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Footer from '@theme-original/Footer';
 import styles from './styles.module.css';
-import CNCF from '@site/static/img/cncf.svg';
+import ThemedImage from '@theme/ThemedImage';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function FooterWrapper(props) {
   return (
     <>
       <section className={[styles.footer_section, styles.section_padding].join(' ')}>
         <h4>We are a <a href="https://www.cncf.io/" target="_blank">Cloud Native Computing Foundation</a> Sandbox Project.</h4>
-        <CNCF title="Cloud Native Computing Foundation" width="100%" />
+        <ThemedImage
+          alt="Docusaurus themed image"
+          sources={{
+            light: useBaseUrl('/img/cncf_light.svg'),
+            dark: useBaseUrl('/img/cncf_dark.svg'),
+          }}
+        />
       </section>
       <Footer {...props} />
     </>
