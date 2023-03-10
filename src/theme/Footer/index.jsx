e13dd31295c9e88e52155fe9eb93294c1acb1ac2
Asmit2952 <asmitbm2952002@gmail.com>
2023-03-10 19:40:18 +0530

    fix padding

--- src/theme/Footer/index.jsx
+++ src/theme/Footer/index.jsx
@@ -6,7 +6,7 @@ import CNCF from '@site/static/img/cncf.svg';
 export default function FooterWrapper(props) {
   return (
     <>
-      <section className={styles.footer_section}>
+      <section className={[styles.footer_section, styles.section_padding].join(' ')}>
         <h4>We are a <a href="https://www.cncf.io/" target="_blank">Cloud Native Computing Foundation</a> Sandbox Project.</h4>
         <CNCF title="Cloud Native Computing Foundation" width="100%" />
       </section>
