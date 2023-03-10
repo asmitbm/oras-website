672e2e7c244f54535ad9e73614ebc9c767fab183
Asmit2952 <asmitbm2952002@gmail.com>
2023-03-10 19:34:24 +0530

    add footer section cncf

---
+++ src/theme/Footer/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '@theme-original/Footer';
+import styles from './styles.module.css';
+import CNCF from '@site/static/img/cncf.svg';
+
+export default function FooterWrapper(props) {
+  return (
+    <>
+      <section className={styles.footer_section}>
+        <h4>We are a <a href="https://www.cncf.io/" target="_blank">Cloud Native Computing Foundation</a> Sandbox Project.</h4>
+        <CNCF title="Cloud Native Computing Foundation" width="100%" />
+      </section>
+      <Footer {...props} />
+    </>
+  );
+}
