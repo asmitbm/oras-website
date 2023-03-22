cc8c765ef20585cdda7d55a7d7d47cb999aa65f4
Asmit2952 <asmitbm2952002@gmail.com>
2023-03-22 21:46:40 +0530

    fix edit this page links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,14 +50,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/asmitbm/oras-website/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/asmitbm/oras-website/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
