8482e41bf67ad6af5627c09d294b0265621a6c55
Asmit2952 <asmitbm2952002@gmail.com>
2023-03-26 23:32:10 +0530

    replaced workflow diagrams

--- src/components/Workflow/index.jsx
+++ src/components/Workflow/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import ORASWorkflow from '@site/static/img/oras_workflow.svg';
+import ThemedImage from '@theme/ThemedImage';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 
 export default function Workflow() {
     return (
         <div className={[styles.workflow, styles.section_padding].join(' ')}>
             <div className={styles.workflow_image}>
-                <ORASWorkflow title="ORAS Workflow" width="100%" />
+                <ThemedImage
+                    alt="ORAS Workflow Image"
+                    sources={{
+                        light: useBaseUrl('/img/oras_workflow_light.svg'),
+                        dark: useBaseUrl('/img/oras_workflow_dark.svg'),
+                    }}
+                />
             </div>
         </div>
     );
